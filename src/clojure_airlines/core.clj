1fd32582df871a9915b0f756bfc0b4029f117c44
anyaachan <anna.danchenko@icloud.com>
2023-11-01 11:29:58 +0100

    add graph-add-edge and graph-add-vertex

--- src/clojure_airlines/core.clj
+++ src/clojure_airlines/core.clj
@@ -14,3 +14,30 @@
 (defrecord Edge [from to label weight])
 (defn make-edge [from to label weight]
   (Edge. from to label weight))
+
+
+
+(defn graph-add-vertex! [graph label]
+  (let [vertices (:vertices graph)
+        new-vertex (make-vertex label)]
+    (dosync
+      (ref-set vertices (assoc @vertices label new-vertex))))
+  nil)
+
+(defn graph-edge-key [from to]
+  (sort (list from to)))
+
+(defn graph-add-edge! [graph from to label weight]
+  (let [vertices (:vertices graph)
+        from-vertex (get @vertices from)
+        to-vertex (get @vertices to)
+        from-vertex-neighbors @(:neighbors from-vertex)
+        to-vertex-neighbors @(:neighbors to-vertex)
+        new-edge (make-edge from to label weight)
+        new-edge-key (graph-edge-key from to)]
+    (dosync
+      ;(println "adding edge: " (:label from-vertex) (:label to-vertex))
+      (ref-set (:edges graph) (assoc @(:edges graph) new-edge-key new-edge))
+      (ref-set (:neighbors from-vertex) (conj from-vertex-neighbors to))
+      (ref-set (:neighbors to-vertex) (conj to-vertex-neighbors from)))))
+
