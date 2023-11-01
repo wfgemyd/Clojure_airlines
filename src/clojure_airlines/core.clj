32e8bcfa4012ed304acabc0169b6cc8fb59cd79a
anyaachan <anna.danchenko@icloud.com>
2023-11-01 11:05:21 +0100

    add data structures

--- src/clojure_airlines/core.clj
+++ src/clojure_airlines/core.clj
@@ -1,7 +1,14 @@
 (ns clojure-airlines.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defrecord Graph [vertices edges])
+(defn make-graph []
+  (Graph. (ref {}) (ref {})))
+
+(defrecord Vertex [label visited neighbors cost-so-far path])
+(defn make-vertex [label]
+  (Vertex. label (ref 0) (ref '()) (ref 0) (ref '())))
+
+(defrecord Edge [from to label weight])
+(defn make-edge [from to label weight]
+  (Edge. from to label weight))
